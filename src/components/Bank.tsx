8296874ff070f19013b90e279c4466572ae82f21
Dulitha Ranatunga <dulitharanatunga@gmail.com>
2023-06-27 22:42:56 +1000

    With circular input

--- src/components/Bank.tsx
+++ src/components/Bank.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography, Box } from '@mui/material';
+import { Button, Typography, Box, Stack } from '@mui/material';
 import { useState } from 'react';
 import {
     CircularInput,
@@ -9,60 +9,56 @@ import {
 
 function Bank() {    
 //   const classes = useStyles();
-  const [value, setValue] = useState(0);
-  const [prevValue, setPrevValue] = useState(0);
-  const [sumValue, setSumValue] = useState(0);
+  const [value, setValue] = useState(0); // Raw Value used in Slider
+  const [prevStep, setPrevStep] = useState(0); // Previous Step
+  const [sumValue, setSumValue] = useState(0);   // Current Step
   const stepValue = (v: number) => Math.round(v * 10) / 10
 
   function updateValue(newVal: number) {
   
-    // const prevValue = Math.round(prev * 10);
+    // const prevStep = Math.round(prev * 10);
     // const actualValue = Math.round(newVal * 10);
     // // if actual = 0 and prev was 10
     // // add 10 to Sum
     // // if actual = 10 and prev was 0
     // // - 10 from Sum
-
-    if (value != newVal) {
+    var newValueAsStep = stepValue(newVal);
+    if (prevStep != newValueAsStep) {
       var total = sumValue;
-      if (newVal === 0 && prevValue === 1) {
+      if (newValueAsStep === 0 && prevStep === 1) {
         total += 1;
       } 
-      if (newVal === 1 && prevValue === 0) {
+      if (newValueAsStep === 1 && prevStep === 0) {
         total -= 1;
       }
-      total += newVal;
       setSumValue(stepValue(total))
-      setPrevValue(value);
+      setPrevStep(newValueAsStep);
     }
     setValue(newVal);    
   }
 
   return (
-    <Box>
-       <Typography variant="h3"> Todays Goal: 100</Typography>
-       <CircularInput
-          value={stepValue(value)}
-          onChange={v => updateValue(stepValue(v))}
-        >
-          <CircularTrack />
-          <CircularProgress />
-          <CircularThumb />
+       <Stack spacing={2} alignItems="center">
+        <Typography variant="h3"> Todays Goal: 100</Typography>
+        <CircularInput
+            value={stepValue(value)}
+            onChange={v => updateValue(stepValue(v))}
+            >
+            <CircularTrack />
+            <CircularProgress />
+            <CircularThumb />
 
-          <text x={200} y={100} textAnchor="middle" dy="0.3em" fontWeight="bold">
-            Value: {value}
-            _PrevValue: {prevValue} 
-            _SumValue: {stepValue(sumValue*10)}  
-            _Display: {stepValue(value*10)} 
-          </text>
-        </CircularInput>
-        <Button variant="contained" color="primary">
-          Bank!
-        </Button>
-        <Button variant="contained" color="secondary">
-          Remove
-        </Button>
-    </Box>
+            <text x={100} y={100} textAnchor="middle" dy="0.3em" fontWeight="bold" fontSize="64px">
+                {stepValue((sumValue + prevStep)*10)} 
+            </text>
+            </CircularInput>
+            <Button variant="contained" color="primary">
+            Bank!
+            </Button>
+            <Button variant="contained" color="secondary">
+            Remove
+            </Button>
+        </Stack>
   );
 }
 
